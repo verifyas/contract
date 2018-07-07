59e9e2170c5f0e72aaefdf355a1bc96c539eb6cf
HassanFahmy <40380244+HassanFahmy@users.noreply.github.com>
2018-07-07 22:46:13 +0200

    Update main_contract.sol

--- main_contract.sol
+++ main_contract.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
-import "https://github.com/verifyas/contract/blob/master/contracts/CREDToken.sol";
-import { SafeMath } from "https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/math/SafeMath.sol";
+import "https://github.com/verifyas/contract/contracts/CREDToken.sol";
+import { SafeMath } from "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract firstbuy {
     
