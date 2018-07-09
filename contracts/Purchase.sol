c5631781016f2f2dbb6125cc9b5c500e5c352483
rickygv99 <rickygv99@yahoo.com>
2018-07-09 14:57:10 -0700

    Record addresses of buyer and seller, Distinguish between Verify and Verify Escrow addresses

--- contracts/Purchase.sol
+++ contracts/Purchase.sol
@@ -5,18 +5,23 @@ import '../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract Purchase {
 
-    address addressVerify; // TODO: Set to Verify Ethereum address
+    address public seller;
+    address public buyer;
+    address public verify; // TODO: Set to Verify Ethereum address
+    address public verifyEscrow; // TODO: Set to Verify Escrow Account Ethereum Address
     uint collectedEther = 0;
 
     modifier onlyVerify {
         require(
-            msg.sender == addressVerify,
+            msg.sender == verify,
             "Only Verify can call this function."
         );
         _;
     }
 
-    constructor () public payable {
+    constructor (address addressSeller) public payable {
+        buyer = msg.sender;
+        seller = addressSeller;
     }
 
     function() external payable {
@@ -27,24 +32,26 @@ contract Purchase {
         if (collectedEther > 0) {
             uint amount = collectedEther;
             collectedEther = 0;
-            addressVerify.transfer(amount);
+            verify.transfer(amount);
         }
     }
 
-    function sendFundsToVerify (address addressSeller, uint creditCeiling) public payable {
+    // TODO: Get creditCeiling from Verify server instead of from buyer
+    function sendFundsToVerify (uint creditCeiling) public payable {
         uint transactionFee = SafeMath.div(msg.value, 100);
         uint payment = SafeMath.sub(msg.value, transactionFee);
 
         transactionFee = toCredToken(transactionFee);
-        // TODO: Transfer transaction fee to Verify
+        // TODO: Transfer transaction fee to Verify account
 
         if (payment <= creditCeiling) {
-            addressSeller.transfer(payment);
+            seller.transfer(payment);
         } else if (creditCeiling < payment) {
             if (creditCeiling > 0) {
-                addressSeller.transfer(creditCeiling);
+                seller.transfer(creditCeiling);
             }
             uint moneyInEscrow = toDaiToken(SafeMath.sub(payment, creditCeiling));
+            // TODO: Transfer escrow money to Verify escrow account
         }
     }
 
