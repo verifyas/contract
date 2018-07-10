391ea98ceb49d41d86acdeb035625fcba5c8fc94
rickygv99 <rickygv99@yahoo.com>
2018-07-10 15:55:22 -0700

    Added unit test of fallback function

--- test/purchase.js
+++ test/purchase.js
@@ -1,20 +1,24 @@
 var Purchase = artifacts.require('Purchase')
 
-contract('Purchase', function ([buyer]) {
-  
+contract('Purchase', function ([buyer, seller, verify, verifyEscrow, donor]) {
+
   var purchase
 
   beforeEach('setup contract for each test', async function () {
-    var addressSeller = '0xf17f52151ebef6c7334fad080c5704d77216b732'
-    var price = 2;
-    var addressVerify = '0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef'
-    var addressVerifyEscrow = '0x821aea9a577a9b44299b9c15c88cf3087f3b5544'
-  
-    purchase = await Purchase.new(addressSeller, price, addressVerify, addressVerifyEscrow)
+    var price = 2
+
+    purchase = await Purchase.new(seller, price, verify, verifyEscrow)
   })
 
   it('has a buyer', async function () {
     assert.equal(await purchase.buyer(), buyer)
+  })
+
+  it('is able to accept ether', async function () {
+    await purchase.sendTransaction({ value: 1e+18, from: donor })
+    
+    var purchaseAddress = await purchase.address
+    assert.equal(web3.eth.getBalance(purchaseAddress).toNumber(), 1e+18)
   })
 
   /*it("should send funds to Verify and Verify escrow", function () {
