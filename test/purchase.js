c6a816a10e47e7c6bd2a9e9b6c191a978e2b6544
rickygv99 <rickygv99@yahoo.com>
2018-07-12 22:13:14 -0700

    WIP bugfixing

--- test/purchase.js
+++ test/purchase.js
@@ -11,63 +11,63 @@ contract('Purchase', function ([buyer, seller, verify, verifyEscrow, donor]) {
     purchase = await Purchase.new(seller, verify, verifyEscrow)
   })
 
-  /*it('has a buyer', async () => {
+  it('has a buyer', async () => {
     assert.equal(await purchase.buyer(), buyer)
   })
 
   it('is able to accept ether', async () => {
     await purchase.sendTransaction({ value: 1e+18, from: donor })
 
-    assert.equal(web3.eth.getBalance(await purchase.address).toNumber(), 1e+18)
+    assert.equal(web3.eth.getBalance(await purchase.address) * 1, 1e+18)
   })
 
   it('permit verify to receive fallback ether', async () => {
-    var verifyInitialBalance = web3.eth.getBalance(verify).toNumber()
+    var verifyInitialBalance = web3.eth.getBalance(verify) * 1
 
     await purchase.sendTransaction({ value: 1e+18, from: donor })
     await purchase.collect({ from: verify })
 
-    assert.equal(web3.eth.getBalance(await purchase.address).toNumber(), 0)
-    assert.isAbove(web3.eth.getBalance(verify).toNumber(), verifyInitialBalance)
+    assert.equal(web3.eth.getBalance(await purchase.address) * 1, 0)
+    assert.isAbove(web3.eth.getBalance(verify) * 1, verifyInitialBalance)
   })
 
   it('buyer should send funds to Verify', async () => {
-    var buyerInitialBalance = web3.eth.getBalance(buyer).toNumber()
-    var verifyInitialBalance = web3.eth.getBalance(verify).toNumber()
+    var buyerInitialBalance = web3.eth.getBalance(buyer) * 1
+    var verifyInitialBalance = web3.eth.getBalance(verify) * 1
 
     await purchase.sendFundsToVerify({ value: web3beta.utils.toWei('3', 'ether') })
 
-    assert.isAbove(web3.eth.getBalance(verify).toNumber(), verifyInitialBalance)
-    assert.isBelow(web3.eth.getBalance(buyer).toNumber(), buyerInitialBalance)
+    assert.isAbove(web3.eth.getBalance(verify) * 1, verifyInitialBalance)
+    assert.isBelow(web3.eth.getBalance(buyer) * 1, buyerInitialBalance)
   })
 
   it('buyer should send funds to Verify escrow', async () => {
-    var buyerInitialBalance = web3.eth.getBalance(buyer).toNumber()
-    var verifyEscrowInitialBalance = web3.eth.getBalance(verifyEscrow).toNumber()
+    var buyerInitialBalance = web3.eth.getBalance(buyer) * 1
+    var verifyEscrowInitialBalance = web3.eth.getBalance(verifyEscrow) * 1
 
     await purchase.sendFundsToVerify({ value: web3beta.utils.toWei('3', 'ether') })
 
-    assert.isAbove(web3.eth.getBalance(verifyEscrow).toNumber(), verifyEscrowInitialBalance)
-    assert.isBelow(web3.eth.getBalance(buyer).toNumber(), buyerInitialBalance)
+    assert.isAbove(web3.eth.getBalance(verifyEscrow) * 1, verifyEscrowInitialBalance)
+    assert.isBelow(web3.eth.getBalance(buyer) * 1, buyerInitialBalance)
   })
 
   it('Verify should send funds to seller', async () => {
-    var sellerInitialBalance = web3.eth.getBalance(seller).toNumber()
-    var verifyEscrowInitialBalance = web3.eth.getBalance(verifyEscrow).toNumber()
+    var sellerInitialBalance = web3.eth.getBalance(seller) * 1
+    var verifyEscrowInitialBalance = web3.eth.getBalance(verifyEscrow) * 1
 
     await purchase.sendFundsToVerify({ value: web3beta.utils.toWei('3', 'ether'), gas: "220000" })
     var result = await purchase.sendFundsToSeller({ value: await purchase.moneyInEscrow(), from: verifyEscrow, gas: "220000" })
 
     var transaction = await web3.eth.getTransaction(result.tx)
-    var gasCost = transaction.gasPrice.mul(result.receipt.gasUsed)
+    var gasCost = transaction.gasPrice.mul(result.receipt.gasUsed * 1)
 
-    assert.isAbove(web3.eth.getBalance(seller).toNumber(), sellerInitialBalance)
-    assert.equal(web3.eth.getBalance(verifyEscrow).toNumber() + gasCost.toNumber(), verifyEscrowInitialBalance)
-  })*/
+    assert.isAbove(web3.eth.getBalance(seller) * 1, sellerInitialBalance)
+    assert.equal(web3.eth.getBalance(verifyEscrow) * 1 + gasCost * 1, verifyEscrowInitialBalance)
+  })
 
   it('seller should refund funds to buyer', async () => {
-    var sellerInitialBalance = await web3.eth.getBalance(seller).toNumber()
-    var buyerInitialBalance = await web3.eth.getBalance(buyer).toNumber()
+    var sellerInitialBalance = await web3.eth.getBalance(seller) * 1
+    var buyerInitialBalance = await web3.eth.getBalance(buyer) * 1
     var transactionFee = web3beta.utils.toWei('3', 'ether') / 100
 
     var resultBuyer = await purchase.sendFundsToVerify({ value: web3beta.utils.toWei('3', 'ether'), gas: "2200000" })
@@ -75,28 +75,28 @@ contract('Purchase', function ([buyer, seller, verify, verifyEscrow, donor]) {
     var resultSeller = await purchase.refundFromSeller({ value: await purchase.paid(), from: seller, gas: "2200000" })
 
     var transactionBuyer = await web3.eth.getTransaction(resultBuyer.tx)
-    var gasCostBuyer = transactionBuyer.gasPrice.mul(resultBuyer.receipt.gasUsed)
+    var gasCostBuyer = transactionBuyer.gasPrice.mul(resultBuyer.receipt.gasUsed * 1)
     var transactionSeller = await web3.eth.getTransaction(resultSeller.tx)
-    var gasCostSeller = transactionSeller.gasPrice.mul(resultSeller.receipt.gasUsed)
+    var gasCostSeller = transactionSeller.gasPrice.mul(resultSeller.receipt.gasUsed * 1)
 
-    assert.equal(web3.eth.getBalance(seller).toNumber() + gasCostSeller.toNumber(), sellerInitialBalance)
-    assert.equal(web3.eth.getBalance(buyer).toNumber() + transactionFee + gasCostBuyer.toNumber(), buyerInitialBalance)
+    assert.equal(web3.eth.getBalance(seller) * 1 + gasCostSeller * 1, sellerInitialBalance)
+    assert.equal(web3.eth.getBalance(buyer) * 1 + transactionFee + gasCostBuyer * 1, buyerInitialBalance)
   })
   
   it('Verify escrow should refund funds to buyer', async () => {
-    var verifyEscrowInitialBalance = web3.eth.getBalance(verifyEscrow).toNumber()
-    var buyerInitialBalance = await web3.eth.getBalance(buyer).toNumber()
+    var verifyEscrowInitialBalance = web3.eth.getBalance(verifyEscrow) * 1
+    var buyerInitialBalance = await web3.eth.getBalance(buyer) * 1
     var transactionFee = web3beta.utils.toWei('3', 'ether') / 100
 
     var resultBuyer = await purchase.sendFundsToVerify({ value: web3beta.utils.toWei('3', 'ether'), gas: "2200000" })
     var resultVerifyEscrow = await purchase.refundFromVerify({ value: await purchase.moneyInEscrow(), from: verifyEscrow, gas: "2200000" })
 
     var transactionBuyer = await web3.eth.getTransaction(resultBuyer.tx)
-    var gasCostBuyer = transactionBuyer.gasPrice.mul(resultBuyer.receipt.gasUsed)
+    var gasCostBuyer = transactionBuyer.gasPrice.mul(resultBuyer.receipt.gasUsed * 1)
     var transactionVerifyEscrow = await web3.eth.getTransaction(resultVerifyEscrow.tx)
-    var gasCostVerifyEscrow = transactionVerifyEscrow.gasPrice.mul(resultVerifyEscrow.receipt.gasUsed)
+    var gasCostVerifyEscrow = transactionVerifyEscrow.gasPrice.mul(resultVerifyEscrow.receipt.gasUsed * 1)
 
-    assert.equal(web3.eth.getBalance(verifyEscrow).toNumber() + gasCostVerifyEscrow.toNumber(), verifyEscrowInitialBalance)
-    assert.equal(web3.eth.getBalance(buyer).toNumber() + transactionFee + gasCostBuyer.toNumber(), buyerInitialBalance)
+    assert.equal(web3.eth.getBalance(verifyEscrow) * 1 + gasCostVerifyEscrow * 1, verifyEscrowInitialBalance)
+    assert.equal(web3.eth.getBalance(buyer) * 1 + transactionFee + gasCostBuyer * 1, buyerInitialBalance)
   })
 })
