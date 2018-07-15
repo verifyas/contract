63808e6d98b695de7c5162bc599fa51a1e77a796
rickygv99 <rickygv99@yahoo.com>
2018-07-15 00:54:17 -0700

    Removed seller refund unit test

--- test/purchase.js
+++ test/purchase.js
@@ -66,24 +66,6 @@ contract('Purchase', function ([buyer, seller, verify, verifyEscrow, donor]) {
     assert.ok(bigInt(web3.eth.getBalance(seller) * 1).greater(sellerInitialBalance))
     assert.ok(bigInt(web3.eth.getBalance(verifyEscrow)).add(gasCost).equals(verifyEscrowInitialBalance))
   })
-
-  it('seller should refund funds to buyer', async () => {
-    var sellerInitialBalance = await web3.eth.getBalance(seller) * 1
-    var buyerInitialBalance = await web3.eth.getBalance(buyer) * 1
-    var transactionFee = web3beta.utils.toWei('3', 'ether') / 100
-
-    var resultBuyer = await purchase.sendFundsToVerify({ value: web3beta.utils.toWei('3', 'ether'), gas: "2200000" })
-    await purchase.sendFundsToSeller({ value: await purchase.moneyInEscrow(), from: verifyEscrow, gas: "2200000" })
-    var resultSeller = await purchase.refundFromSeller({ value: await purchase.paid(), from: seller, gas: "2200000" })
-
-    var transactionBuyer = await web3.eth.getTransaction(resultBuyer.tx)
-    var gasCostBuyer = transactionBuyer.gasPrice.mul(resultBuyer.receipt.gasUsed)
-    var transactionSeller = await web3.eth.getTransaction(resultSeller.tx)
-    var gasCostSeller = transactionSeller.gasPrice.mul(resultSeller.receipt.gasUsed)
-
-    assert.ok(bigInt(web3.eth.getBalance(seller)).add(gasCostSeller).equals(sellerInitialBalance))
-    assert.ok(bigInt(web3.eth.getBalance(buyer)).add(transactionFee).add(gasCostBuyer).equals(buyerInitialBalance))
-  })
   
   it('Verify escrow should refund funds to buyer', async () => {
     var verifyEscrowInitialBalance = web3.eth.getBalance(verifyEscrow) * 1
