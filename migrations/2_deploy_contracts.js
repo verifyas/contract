988674aa7811cb88c3c4aaad1b112607fe377513
Yazin <yazin@verify.as>
2018-10-09 20:49:28 +0300

    Use single contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var Purchase = artifacts.require("./Purchase.sol");
 
 module.exports = function(deployer) {
-  var addressSeller = "0x627306090abab3a6e1400e9345bc60c78a8bef57"
-
-  deployer.deploy(Purchase, addressSeller);
+  deployer.deploy(Purchase);
 };
